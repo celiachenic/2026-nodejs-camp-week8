1b9d849ee92687cdd73c235f8a220a5dedf0c196
msnigatsu <msnigatsu@gmail.com>
2026-08-20 15:50:07 +0800

    刪除註解

--- livefit/entities/Course.js
+++ livefit/entities/Course.js
@@ -1,13 +1,3 @@
-//  **`COURSE`（課程）**
-//   - 欄位：`name` varchar(100) 必填、
-// `description` text 必填、
-// `start_at` timestamp 必填、
-// `end_at` timestamp 必填、
-// `max_participants` integer 必填、
-// `created_at`、`updated_at`（建立／更新時間，由系統自動帶入）
-
-//   - 關聯：`user_id` → `USER`（開課教練）、
-// `skill_id` → `SKILL`（課程技能）
 const { EntitySchema } = require("typeorm");
 module.exports = new EntitySchema({
   name: "Course",
--- livefit/entities/Skill.js
+++ livefit/entities/Skill.js
@@ -1,6 +1,3 @@
-//  **`SKILL`（技能）**
-//   - 欄位：`name` varchar(50) 必填且唯一
-//   - 關聯：無
 const { EntitySchema } = require("typeorm");
 module.exports = new EntitySchema({
   name: "Skill",
